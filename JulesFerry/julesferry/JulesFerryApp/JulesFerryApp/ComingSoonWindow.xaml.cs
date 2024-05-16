494cf085dfb48677bcaad4fbf2318023ee85c3cc
infinuts <163611744+infinuts@users.noreply.github.com>
2024-05-16 20:29:44 +0100

    Add files via upload

---
+++ JulesFerry/julesferry/JulesFerryApp/JulesFerryApp/ComingSoonWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace JulesFerryApp
+{
+    public partial class ComingSoonWindow : Window
+    {
+        public ComingSoonWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
