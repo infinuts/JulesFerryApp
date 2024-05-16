dd6f12ae126921a033e985f5a01185967868740b
infinuts <163611744+infinuts@users.noreply.github.com>
2024-05-16 20:26:54 +0100

    Add files via upload

---
+++ julesferry/JulesFerryApp/JulesFerryApp/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace JulesFerryApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var comingSoonWindow = new ComingSoonWindow();
+            comingSoonWindow.Show();
+            this.Close();
+        }
+
+        private void SignUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            var comingSoonWindow = new ComingSoonWindow();
+            comingSoonWindow.Show();
+            this.Close();
+        }
+
+        private void GoToWebsiteButton_Click(object sender, RoutedEventArgs e)
+        {
+            string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string filePath = Path.Combine(currentDirectory, "Website.html");
+
+            if (File.Exists(filePath))
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = filePath,
+                    UseShellExecute = true
+                });
+            }
+            else
+            {
+                MessageBox.Show("Website.html not found!");
+            }
+        }
+    }
+}
